3b17ee4efbe384de44bd53801e2d8e74bea84772
Thomas <thomas.oberliessen@googlemail.com>
2023-06-04 14:47:44 +0200

    Add statement classes

---
+++ src/main/java/com/craftinginterpreters/lox/Stmt.java
@@ -0,0 +1,36 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+abstract class Stmt {
+  interface Visitor<R> {
+    R visitExpressionStmt(Expression stmt);
+    R visitPrintStmt(Print stmt);
+  }
+  static class Expression extends Stmt {
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitExpressionStmt(this);
+    }
+    final Expr expression;
+
+    Expression(Expr expression) {
+      this.expression = expression;
+    }
+  }
+  static class Print extends Stmt {
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitPrintStmt(this);
+    }
+    final Expr expression;
+
+    Print(Expr expression) {
+      this.expression = expression;
+    }
+  }
+
+  abstract <R> R accept(Visitor<R> visitor);
+}
--- src/main/java/com/craftinginterpreters/lox/tool/GenerateAst.java
+++ src/main/java/com/craftinginterpreters/lox/tool/GenerateAst.java
@@ -20,6 +20,10 @@ public class GenerateAst {
                 "Unary    : Token operator, Expr right"
         ));
 
+        defineAst(outputDir, "Stmt", Arrays.asList(
+                "Expression : Expr expression",
+                "Print      : Expr expression"
+        ));
     }
 
     private static void defineAst(
