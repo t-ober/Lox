fa7663500d036a49e5e249f7186533befafac1d1
Thomas <thomas.oberliessen@googlemail.com>
2023-06-26 07:44:52 +0200

    Add conditional execution

--- src/main/java/com/craftinginterpreters/lox/Expr.java
+++ src/main/java/com/craftinginterpreters/lox/Expr.java
@@ -8,6 +8,7 @@ abstract class Expr {
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
+    R visitLogicalExpr(Logical expr);
     R visitUnaryExpr(Unary expr);
     R visitVariableExpr(Variable expr);
   }
@@ -63,6 +64,22 @@ abstract class Expr {
 
     Literal(Object value) {
       this.value = value;
+    }
+  }
+  static class Logical extends Expr {
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLogicalExpr(this);
+    }
+    final Expr left;
+    final Token operator;
+    final Expr right;
+
+    Logical(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
     }
   }
   static class Unary extends Expr {
--- src/main/java/com/craftinginterpreters/lox/Parser.java
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -52,8 +52,23 @@ class Parser {
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block((block()));
+        if (match(IF)) return ifStatement();
 
         return expressionStatement();
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'if'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private Stmt printStatement() {
@@ -85,7 +100,7 @@ class Parser {
     }
 
     private Expr assignment() {
-        Expr expr = equality();
+        Expr expr = or();
 
         if (match(EQUAL)) {
             Token equals = previous();
@@ -102,6 +117,29 @@ class Parser {
         return expr;
     }
 
+    private Expr or() {
+        Expr expr = and();
+
+        while (match(OR)) {
+            Token operator = previous();
+            Expr right = and();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr and() {
+        Expr expr = equality();
+
+        while (match(AND)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+
+        return expr;
+    }
 
     private Expr equality() {
         Expr expr = comparison();
